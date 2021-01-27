156312ab955a8920d021af567ecc614b43c15a77
Josh Nylander <josh@nylanderfamily.org>
2021-01-26 21:23:39 -0800

    Improvements to build script but still not ready..

--- build.ps1
+++ build.ps1
@@ -1,7 +1,48 @@
+param (
+    [String] $Path = ".\PoshIssues", #Path to the module folder
+    [String] $TestsPath = ".\Tests", #Path to the Pester tests files
+    [String] $DocsPath = ".\docs", #Path to the platyPS docs
+    [String] $PublishRepo = "PSGallery"
+)
+
+Import-Module -Name $Path -Force
+. .\localTestValues.ps1
+
+# Run tests
+Invoke-Pester -Script "$TestsPath*"
+
+#TODO: verify tests past and then proceed, if not, stop here
+
+
+# Build help files
+Import-Module platyPS
+New-ExternalHelp $DocsPath -OutputPath "$Path\en-US" -Force
+
+# Publish to PSGallery
 Import-Module PowerShellGet
+Publish-Module -Path $Path -Repository $PublishRepo -Verbose
 
-#Publish to PSGallery and install/import locally
+<#
 
-Publish-Module -Path .\PoshIssues -Repository PSGallery -Verbose
-Install-Module -Name PoshIssues -Repository PSGallery -Force
-Import-Module -Name PoshIssues -Force
+{
+    "version": "0.1.0",
+    "command": "powershell",
+    "args": [   
+        "-ExecutionPolicy",
+        "Unrestricted",
+        "-NoProfile",
+        "-File",
+        "${cwd}/build.ps1"       
+    ],
+    "taskSelector": "-task ",
+    "showOutput": "always",
+    "tasks": [
+        {
+            "taskName": "build",
+            "showOutput": "always",
+            "isBuildCommand": true
+        }
+    ]
+}
+
+#>
