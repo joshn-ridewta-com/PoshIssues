00a2b69e4b3fbfb9648e2f428c37377117653729
Josh Nylander <josh@nylanderfamily.org>
2021-01-26 21:24:27 -0800

    replaced by build.ps1

--- update-docs.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [String] $Path = ".\docs"
-)
-Import-Module platyPS
-
-#Import-Module .\PoshIssues -Force
-# Update-MarkdownHelpModule -Path $Path -Force
-
-New-ExternalHelp .\docs -OutputPath .\PoshIssues\en-US -Force
